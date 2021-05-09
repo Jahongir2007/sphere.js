e596a622c119cd12d75e4d691f787ed78029b89a
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-05-09 19:55:17 +0500

    Update sphere.js

--- bin/sphere.js
+++ bin/sphere.js
@@ -1,12 +1,13 @@
 /*
 Author: Jahongir Sobirov
 Sphere.js mini web framework
-Version: 1.0.0
+Version: 1.4.3
 License: MIT
 */
 const http = require("http");
 const https = require('https');
 const fs = require("fs");
+const url = require("url");
 
 function options(self){
     const serverSettings = function(req, res){
@@ -58,9 +59,38 @@ function post(postself){
       req.write(data)
       req.end()
 }
-
+function queryURL(self){
+  http.createServer(function(req, res){
+    res.writeHead(self["head"], {"Content-type": "text/html"});
+    res.write(req.url);
+    res.end();
+  }).listen(self["listen"])
+}
+function splitURL(self){
+  http.createServer(function(req, res){
+    res.writeHead(self["head"], {"Content-type": "tex/html"});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end();
+  }).listen(self["listen"])
+}
+function URL(self, adr){
+  var q = url.parse(adr["link"], true);
+  if(self == "host"){
+    console.log(q.host);
+  }
+  else if(self == "pathname"){
+    console.log(q.pathname);
+  }
+  else if(self == "search"){
+    console.log(q.search);
+  }
+}
 module.exports = {
     options,
     html,
-    post
+    post,
+    queryURL,
+    splitURL,
+    URL
 }
